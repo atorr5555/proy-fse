0d0e4ce136cddea1a6e59181e23880d5e889fb2f
Rodrigopinon12 <rodrigo_pinon2-a@hotmail.com>
2021-11-17 20:06:18 -0600

    cambios a 2 scripts

--- proy.py
+++ proy.py
@@ -1,12 +1,11 @@
 from gpiozero import DistanceSensor, PWMLED,MotionSensor,Servo,Button
-from signal import pause
 import telebot
 from time import sleep
 from bluedot import BlueDot
 import subprocess
 from multiprocessing import Process,Queue,Pipe
-import os
 import Adafruit_DHT 
+import os
 
 #Definimos las varibale a utilizar en las diferentes funciones del proyecto WORKY
 bd = BlueDot()
@@ -21,49 +20,6 @@ bot=telebot.TeleBot(API_TOKEN)
 modo_seguro=False
 banderaViolacion=False
 
-#FUNCIONES para las caracteristicas de WORKY
-def inRange():
-    led[0].on()
-    #bot.send_message('1157726974','Se detecto movimiento, Alarma ENCENDIDA')
-    #bot.send_message('@atorr55','Se detecto movimiento, Alarma ENCENDIDA')
-    bot.send_message('-683852329','Se detecto movimiento, Alarma ENCENDIDA')
-    return True
-    
-def outOfRange():
-    led[0].off()
-    return True
-
-def turnon():
-    print('TURN OONN')
-    print(modo_seguro)
-    if(modo_seguro):
-        bot.send_message('-683852329','Se detecto movimiento, Alarma ENCENDIDA')
-        os.system("sudo python ~/lightshowpi/py/synchronized_lights.py --file=/home/pi/Downloads/sonido.mp3")
-        banderaViolacion=True
-    else:
-        servo.max()
-        print('ABIERTA')
-        while(not(pir.motion_detected)):
-            servo.max()
-        servo.min()
-        print('CERRADA')
-
-def checarAlarma():
-    if(modo_seguro and banderaViolacion):
-        os.system("sudo python ~/lightshowpi/py/synchronized_lights.py --file=/home/pi/Downloads/sonido.mp3")
-
-def comando(pos):
-    if pos.bottom:
-        led = 0
-        if(pos.x>0):
-            led=1 #Enciende luz de cuarto
-        if(pos.x<0):
-            led=0 #Enciende luz de baño
-        brightness = (pos.y + 1) / 2
-        leds[led].value = brightness
-    elif pos.top:
-        #Llamado al asistente con bandera de solo 1 instruccion
-        subprocess.call("python pushToTalk.py --once", shell=True)
 
 #Intrucciones ingresadas por medio de TELEGRAM escrito
 @bot.message_handler(commands=['Desactivar_seguridad'])
@@ -98,11 +54,8 @@ Se ajusto la luz del baño
 @bot.message_handler(commands=['ApagarLuces'])
 def send_welcome(message):
     bot.reply_to(message, """\
-/EncenderBaño
+Se apagaron las luces
 """)
-    #bot.reply_to(message, """\
-#Se ajusto la luz del baño
-#""")
     leds[0].value=0
     leds[1].value=0
 
@@ -136,29 +89,6 @@ Se ajusto la luz del baño
     else:
         valor=message.text.split()[1:][0]
         leds[0].value=int(valor)/100
-
-
-#Ejecucion por medio de BLUEDOT 
-bd.when_pressed = comando
-
-#Sensor de proximidad
-pir.when_motion=turnon
-
-#Cuando la seguridad haya sido violada seguir activando la alarma
-checarAlarma()
-
-#Switch
-if(buttonBaño.is_pressed):
-    if(leds[0].value==0):
-        leds[0].value=1
-    else:
-        leds[0].value=0
-
-if(buttonCuarto.is_pressed):
-    if(leds[1].value==0):
-        leds[1].value=1
-    else:
-        leds[1].value=0
 
 #Mantener siempre activo al bot programa en eterna ejecución
 bot.infinity_polling()
---
+++ sensoresBT.py
@@ -0,0 +1,74 @@
+from gpiozero import DistanceSensor, PWMLED,MotionSensor,Servo,Button
+from gpiozero import DistanceSensor, PWMLED,MotionSensor,Servo,Button
+import telebot
+from bluedot import BlueDot
+import Adafruit_DHT 
+import os
+
+bd = BlueDot()
+leds = [PWMLED(17), PWMLED(27)] #Los de cuarto y baño
+pir = MotionSensor(21)
+buttonCuarto = Button(2)
+buttonBaño = Button(3)
+servo=Servo(4)
+sensorTemp=Adafruit_DHT.DHT11
+API_TOKEN=""
+bot=telebot.TeleBot(API_TOKEN)
+modo_seguro=False
+
+#TXT o base de datos [modo_seguro]
+
+
+#Funciones
+def turnon():
+    if(modo_seguro):
+        bot.send_message('-683852329','Se detecto movimiento, Alarma ENCENDIDA')
+        os.system("sudo python ~/lightshowpi/py/synchronized_lights.py --file=/home/pi/Downloads/sonido.mp3")
+        banderaViolacion=True
+    else:
+        servo.max()
+        print('ABIERTA')
+        while(not(pir.motion_detected)):
+            servo.max()
+        servo.min()
+        print('CERRADA')
+
+def checarAlarma():
+    if(modo_seguro and banderaViolacion):
+        os.system("sudo python ~/lightshowpi/py/synchronized_lights.py --file=/home/pi/Downloads/sonido.mp3")
+
+def comando(pos):
+    if pos.bottom:
+        led = 0
+        if(pos.x>0):
+            led=1 #Enciende luz de cuarto
+        if(pos.x<0):
+            led=0 #Enciende luz de baño
+        brightness = (pos.y + 1) / 2
+        leds[led].value = brightness
+    elif pos.top:
+        #Llamado al asistente con bandera de solo 1 instruccion
+        subprocess.call("python pushToTalk.py --once", shell=True)
+
+while True:
+    #Ejecucion por medio de BLUEDOT 
+    bd.when_pressed = comando
+
+    #Sensor de proximidad
+    pir.when_motion=turnon
+
+    #Cuando la seguridad haya sido violada seguir activando la alarma
+    checarAlarma()
+
+    #Switch
+    if(buttonBaño.is_pressed):
+        if(leds[0].value==0):
+            leds[0].value=1
+        else:
+            leds[0].value=0
+
+    if(buttonCuarto.is_pressed):
+        if(leds[1].value==0):
+            leds[1].value=1
+        else:
+            leds[1].value=0
