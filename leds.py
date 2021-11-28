5c9407bca47fcd8d0e17461c86d149093acbb904
atorr5555 <antoniotorr5555@gmail.com>
2021-11-28 04:02:26 -0600

    Leds mapeados correctos

--- leds.py
+++ leds.py
@@ -8,15 +8,15 @@ buttonBaño = Button(3)
 #Switch
 while True:
     if(buttonBaño.is_pressed):
-        if(leds[0].value==0):
-            leds[0].value=1
-        else:
-            leds[0].value=0
-        sleep(1)
-
-    if(buttonCuarto.is_pressed):
         if(leds[1].value==0):
             leds[1].value=1
         else:
             leds[1].value=0
+        sleep(1)
+
+    if(buttonCuarto.is_pressed):
+        if(leds[0].value==0):
+            leds[0].value=1
+        else:
+            leds[0].value=0
         sleep(1)
